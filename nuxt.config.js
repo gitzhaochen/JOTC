d1b37a833ab2d3b58dd7540f94e61cb73f99079c
zhaochen <zhaochen@yuanben.org>
2019-08-05 20:05:00 +0800

    config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,26 +4,43 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'jotc',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+        name: 'renderer',
+        content: 'webkit'
+      },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+      },
+      { hid: 'keywords', name: 'keywords', content: '' },
+      { hid: 'description', name: 'description', content: '' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' }
+    ]
+  },
+  server: {
+    port: 5555,
+    host: '0.0.0.0'
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: '@/components/loading.vue',
   /*
    ** Global CSS
    */
-  css: [],
+  // 配置所有页面渲染后滚动至顶部
+  router: {
+    scrollBehavior(to, from, savedPosition) {
+      return { x: 0, y: 0 }
+    }
+  },
+
+  // 引入样式文件
+  css: ['./assets/style/reset.css', './assets/style/main.less'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -33,12 +50,15 @@ module.exports = {
    */
   devModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    ['@nuxtjs/eslint-module']
   ],
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
+  styleResources: {
+    less: ['./assets/style/vars.less', './assets/style/mixins.less']
+  },
   /*
    ** Build configuration
    */
@@ -46,6 +66,31 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extractCSS: process.env.NODE_ENV === 'production', // 生产环境单独抽离css
+    filenames: {
+      app: ({ isDev }) => (isDev ? '[name].js' : '[name].[chunkhash:7].js'),
+      chunk: ({ isDev }) => (isDev ? '[name].js' : '[name].[chunkhash:7].js'),
+      css: ({ isDev }) => (isDev ? '[name].js' : '[name].[contenthash:7].css'),
+      img: ({ isDev }) =>
+        isDev ? '[name].[ext]' : 'img/[name].[hash:7].[ext]',
+      font: ({ isDev }) =>
+        isDev ? '[name].[ext]' : 'fonts/[name].[hash:7].[ext]'
+    },
+    babel: {
+      presets({ isServer }) {
+        const targets = isServer
+          ? { node: 'current' }
+          : { ie: '9', edge: '17', chrome: '44', firefox: '60', safari: '7' }
+        return [[require.resolve('@nuxt/babel-preset-app'), { targets }]]
+      }
+    },
+    extend(config, ctx) {
+      // 开启source-map
+      if (ctx.isDev) {
+        config.devtool = 'cheap-module-eval-source-map'
+      } else {
+        config.devtool = 'cheap-module-source-map'
+      }
+    }
   }
 }
