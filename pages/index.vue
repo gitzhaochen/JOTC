a5855106512cbecfc4551f79d3a64d426268c25d
zhaochen <zhaochen@yuanben.org>
2019-08-06 13:31:05 +0800

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="container">
+    <Logo></Logo>
     <div>
-      <logo />
-      <h1 class="title">
-        jotc
-      </h1>
-      <h2 class="subtitle"></h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      哈哈哈哈哈
     </div>
   </div>
 </template>
